b714ec99df8dd7c5642f54026059beb8fe1febb5
Ubuntu <ubuntu@ip-172-31-89-143.ec2.internal>
2020-09-13 01:28:26 +0000

    inserted data

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import urllib.request
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
 import json
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'pennapps'
+app.config['MONGO_URI'] = 'mongodb+srv://pennapps:.@cluster0.cpkso.mongodb.net/pennapps?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
---
+++ insertdata.py
@@ -0,0 +1,71 @@
+import json, pymongo, sys
+  
+# Opening JSON file 
+f = open(sys.argv[1]) 
+  
+# returns JSON object as  
+# a dictionary
+
+data = json.load(f) 
+
+client = pymongo.MongoClient('mongodb+srv://pennapps:.@cluster0.cpkso.mongodb.net/pennapps?retryWrites=true&w=majority')
+db = client.pennapps
+collection = db.facemasks
+
+def parse_amazondata(data):
+    for i in data["image"]:
+        if 'amount' in i:
+            amountlist = i["amount"]
+            value = ""
+            for j in amountlist:
+                value += j["name"]
+            _data = {}
+            _data["store"] = "amazon"
+            _data["item"] = "facemasks"
+            _data["image"] = i["image"]
+            _data["url"] = i["url"]
+            _data["label"] = i["label"]
+            _data["value"] = value
+            post_id = collection.insert_one(_data).inserted_id
+            post_id
+            
+def parse_walmartdata(data):
+    for i in data["image"]:
+        if 'amount' in i:
+            amountlist = i["amount"]
+            value = ""
+            for j in amountlist:
+                value += j["name"]
+            _data = {}
+            _data["store"] = "walmart"
+            _data["item"] = "facemasks"
+            _data["image"] = i["image"]
+            _data["url"] = i["url"]
+            _data["label"] = i["label"]
+            _data["value"] = value
+            post_id = collection.insert_one(_data).inserted_id
+            post_id
+            
+def parse_ebaydata(data):
+    for i in data["image"]:
+        _data = {}
+        _data["store"] = "ebay"
+        _data["item"] = "facemasks"
+        _data["image"] = i["image"]
+        _data["url"] = i["url"]
+        _data["label"] = i["label"]
+        _data["value"] = i["amount"]
+        post_id = collection.insert_one(_data).inserted_id
+        post_id
+            
+print(sys.argv)
+if sys.argv[2] == "amazon":
+    parse_amazondata(data)
+if sys.argv[2] == "walmart":
+    parse_walmartdata(data)
+if sys.argv[2] == "ebay":
+    parse_ebaydata(data)
+    
+#if sys.argv[2] = "walmart":    
+    
+
